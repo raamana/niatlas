90b87691aae996e806b18168ff008de5ab0b91b2
Pradeep Reddy Raamana <raamana@gmail.com>
2019-07-11 09:45:52 -0400

    fixing setup

--- setup.py
+++ setup.py
@@ -5,12 +5,6 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [ ]
 
 setup_requirements = ['pytest-runner', ]
@@ -36,7 +30,7 @@ setup(
     description="atlas classes and methods for neuroimaging",
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description="atlas classes and methods for neuroimaging",
     include_package_data=True,
     keywords='niatlas',
     name='niatlas',
